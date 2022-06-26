959975d9122bbbda0d7061f0b21219b6909993cc
Marian Kuzyk <mariankuzyk@gmail.com>
2022-06-26 20:36:49 +0300

    finally

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,40 +1,52 @@
 import { useState } from "react";
 import s from './ContactForm.module.css'
 
-export default function ContactForm() {
-    
-    state = {
-        name: '',
-        number: '',
-    }
+// onSubmit це пропси які приймаються тут
+export default function ContactForm({onSubmit}) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-    handleChange = e => {
-        const { name, value } = e.currentTarget;
-        this.setState({ [name]: value })
+    const handleChange = e => {
+        const { name, value } = e.currentTarget; // отримую значення в імпуті
+
+        // залежно від того яке поле міняється, то я записую в стейт
+        switch (name) {
+            case "name":
+                setName(value)
+                break;
+            
+            case "number":
+                setNumber(value);
+                break;
+        
+            default:
+                break;
+        }
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
-        this.props.onSubmit(this.state);
-        this.reset();
+        // приймаю пропси і записую їх в стейт при сабміті
+        onSubmit({ name, number});
+
+        reset();
     };
 
-    reset = () => {
-        this.setState({
-            name: '',
-            number: ''
-        });
+    const reset = () => {
+        // очищую імпути
+        setName('');
+        setNumber('');
     };
 
     return (
-        <form onSubmit={this.handleSubmit} className={s.form}>
+        <form onSubmit={handleSubmit} className={s.form}>
             <label className={s.label}> Name
                 <input
                     type="text"
                     name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
+                    value={name}
+                    onChange={handleChange}
                     className={s.input}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -45,8 +57,8 @@ export default function ContactForm() {
                 <input
                     type="tel"
                     name="number"
-                    value={this.state.number}
-                    onChange={this.handleChange}
+                    value={number}
+                    onChange={handleChange}
                     className={s.input}
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
